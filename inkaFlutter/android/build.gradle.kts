1c255dce91efa9944cd5412248953bcc8acc92e7
NachoGarRen <nachogarren@campus.monlau.com>
2026-05-19 18:38:55 +0200

    Fix Android release build: bump compileSdk/targetSdk to 36 and fix Kotlin DSL override timing

--- inkaFlutter/android/build.gradle.kts
+++ inkaFlutter/android/build.gradle.kts
@@ -16,6 +16,24 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    println("=== Subproject registered: ${project.name} ===")
+    afterEvaluate {
+        plugins.withId("com.android.library") {
+            configure<com.android.build.gradle.BaseExtension> {
+                println("=== Gradle Override (afterEvaluate): Setting compileSdkVersion to 36 for library subproject: ${project.name} ===")
+                compileSdkVersion(36)
+            }
+        }
+        plugins.withId("com.android.application") {
+            configure<com.android.build.gradle.BaseExtension> {
+                println("=== Gradle Override (afterEvaluate): Setting compileSdkVersion to 36 for application subproject: ${project.name} ===")
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
+
+subprojects {
     project.evaluationDependsOn(":app")
 }
 
